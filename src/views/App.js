b93e6aa1f52fde6d6ce9e39374dd7a422299916a
Ashot Nazaryan <contact@hego.co>
2016-06-02 14:28:39 -0700

    Updates

--- src/views/App.js
+++ src/views/App.js
@@ -1,34 +1,95 @@
 import { Component } from 'react'
 import Sidebar from 'react-sidebar'
 
+// const defaultStyles = {
+// 	wrapper: {
+// 		backgroundColor: '#252527',
+// 		height: '100%'
+// 	},
+// 	table: {
+// 		width: '100%',
+// 		color: 'white',
+// 		textAlign: 'center',
+// 		'td': {
+// 			padding: '20px 0'
+// 		}
+// 	}
+// };
+
+
+
+
+
 module.exports = class App extends Component {
 	constructor (props) {
 		super(props)
-    this.state = {
-      sidebarOpen: false
-    }
+
+		this.state = {
+			sidebarOpen: true,
+			docked: true,
+			transitions: false
+		}
 	}
 
-  onSetSidebarOpen (open) {
-    this.setState({sidebarOpen: open});
-  }
+
 
 	render () {
-    var sidebarContent = <b>Sidebar content</b>;
-    return (
-      <Sidebar sidebar={sidebarContent}
-       open={this.state.sidebarOpen}
-       onSetOpen={this.onSetSidebarOpen}>
-        <div>
-          <p>
-            Hello, <input type="text" placeholder="Your name here" />!
-          </p>
-          <p>
-            Sup
-          </p>
-        </div>
-      </Sidebar>
+		const sidebarContent = <div>Ampact</div>;
 
-    );
+		let Song = [
+			{
+				Name: "AYY",
+				Artist: "YOO",
+				Album: "OHH"
+			},
+			{
+				Name: "Name",
+				Artist: "Artist",
+				Album: "Album"
+			},
+			{
+				Name: "Name",
+				Artist: "Artist",
+				Album: "Album"
+			},
+			{
+				Name: "Name",
+				Artist: "YOO",
+				Album: "Album"
+			}
+		]
+		let mainContent = 
+											<div className='wrapper' /*style={defaultStyles.wrapper}*/>
+												<table /*style={defaultStyles.table}*/>
+													<thead>
+														<tr>
+															<th>Song</th>
+															<th>Artist</th>
+															<th>Album</th>
+														</tr>
+													</thead>
+													<tbody>
+														{Song.map(function(object, i){
+      												 return <tr>
+	      												  <td>{object.Name}</td>
+	      												  <td>{object.Artist}</td>
+	      												  <td>{object.Album}</td>
+      												  </tr>
+														})}
+													</tbody>
+												</table>
+											</div>
+
+							
+
+		return (
+			<Sidebar sidebar={sidebarContent}
+			 open={this.state.sidebarOpen}
+			 docked={this.state.docked}
+			 transitions={this.state.transitions}>
+				{mainContent}
+			</Sidebar>
+
+		);
 	}
 }
